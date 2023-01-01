4ce12a5c9125cedc0d0ba584444a6865396923ec
Dmitry V. Levin <ldv@altlinux.org>
2023-01-01 08:00:00 +0000

    Update copyright years
    * config.guess: Update copyright years.
    * config.sub: Likewise.
    * testsuite/config-guess.sh: Likewise.
    * testsuite/config-sub.sh: Likewise.
    * doc/config.guess.1: Regenerate.
    * doc/config.sub.1: Likewise.

--- testsuite/config-sub.sh
+++ testsuite/config-sub.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2004-2022 Free Software Foundation, Inc.
+# Copyright 2004-2023 Free Software Foundation, Inc.
 # Contributed by Ben Elliston <bje@gnu.org>.
 #
 # This test reads pairs from config-sub.data: an alias and its
